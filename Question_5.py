4918e9f12d6f8f7e4c3c8787c704dcb8c8dac079
samuelckraft <samuelckraft@gmail.com>
2024-05-04 13:04:39 -0400

    update

--- Question_5.py
+++ Question_5.py
@@ -2,3 +2,16 @@
 
 catalog1 = (("Laptop", 1000), ("Camera", 500))
 catalog2 = (("Smartphone", 800), ("Tablet", 450))
+
+
+def join_catalogs(*catalogs):
+    combined = ()
+    for catalog in catalogs:
+        combined += catalog
+    print("\nCatalog: ")
+    for set in combined:
+        item, price = set
+        print(f"{item} - {price}")
+        
+
+join_catalogs(catalog1, catalog2)
